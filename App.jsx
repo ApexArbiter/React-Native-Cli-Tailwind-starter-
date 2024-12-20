5c972edb2c4497b2e407083e53d290207fd20628
ApexArbiter <mahadwebdev@gmail.com>
2024-12-20 18:45:39 +0500

    2nd commit

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+const App = () => (
+  <View className="flex-1 items-center justify-center bg-blue-500">
+    <Text className="text-white text-lg font-bold">Hello, Tailwind!</Text>
+  </View>
+);
+
+export default App;
+
